d2e06d6f5f2b66516caf51fdb05578942d30d2bc
Andres Hernandez <pipehernandez1998@gmail.com>
2024-04-25 16:49:08 -0500

    Test done

--- Viaje Macondo Hernan/script/script.js
+++ Viaje Macondo Hernan/script/script.js
@@ -1,6 +1,6 @@
 // Functions
 // Global Variables
-let characteristics = {name:"Hildebrando", budget:0,live:true,}
+let characteristics = {name:"Hildebrando", budget:0,live:true}
 let counter = 0
 let flag = true
 let spendMoney = 0
@@ -11,15 +11,15 @@ let data = ()=>{
 // StaringTrip  case 1
 let staringTrip = () =>{
     let flag = true
-    alert("Friday, Hildebrando arrives at El Dorado Airport. He wants to buy some food before the flight since he has a layover in Medellin in the early morning, and he would arrive in Macondo in the morning")
+    alert("Friday, Hildebrando arrives at El Dorado Airport,He wants to buy some food before the flight since he has a layover in Medellin in the early morning.")
     while(flag){
-        let opcions = parseInt(prompt("🍔🍔🍔What do you want to do? \n 1  buy almojábana for 15.000 \n 2  buy Subway for 23.000 \n 3  to wait and buy something in medellin"))    
+        let opcions = parseInt(prompt("🍔What do you want to do?🍔 \n        1 to buy almojábana for 15.000 \n        2  to buy Subway for 23.000 \n        3  to wait and buy something in medellin"))    
         if(!isNaN(opcions)){
             switch(opcions){
                 case 1: 
                 characteristics.budget-=15000
                 spendMoney+=15000
-                alert("The almojabana was bad for you and your health was affected")
+                alert("The almojabana was bad for you and you are not feelling well")
                 flag = false
                 break
                 
@@ -43,6 +43,47 @@ let staringTrip = () =>{
     }
 
 }  
+let medellinStop =() =>{
+ if(spendMoney === 0){
+    let flag = true
+    alert("Medellin, Hildebrando arrives at Medellin aeroport. He wants to buy some food because he didn't eat something in the previous aeroport")
+    while(flag){
+        let opcions = parseInt(prompt("🥓What do you want to do?🥓 \n 1 to buy Chicharron for $150.000 \n 2 to buy Macondo food for $100.000 \n 3 to buy Empanada $2000"))    
+        if(!isNaN(opcions)){
+            switch(opcions){
+                case 1: 
+                characteristics.budget-=150000
+                spendMoney+=150000
+                alert("The Chicharron was good for you and you are feelling very well")
+                flag = false
+                break
+                
+                case 2: 
+                characteristics.budget-=1000000
+                spendMoney+=1000000
+                alert("Very delicuous 👌")
+                flag = false
+                break
+
+                case 3:
+                alert("The empanada it was bad for you, and you spend in the farmaccy $100.000")
+                characteristics.budget-=102000
+                spendMoney+=102000
+                flag = false
+                break
+            }
+        
+        }
+        else{
+            alert("Enter a valid opcion")
+        }
+    }
+
+ }
+ else{
+    return
+ }
+}
 // Suit Case required  case 2
 let reduceSuitCase =()=>{
 alert("Hildebrando arrives at the waiting area and checks his suitcase, which does not meet the carry-on size requirements.")
@@ -90,7 +131,7 @@ let descryptBinary = ()=>{
     
      let final_convert = character.join("")
     
-    alert(` finally Hilderbrando resolve The password and continue to book the hotel room, the password is ${final_convert}`)
+    alert(`Finally Hilderbrando resolve The password and continue to book the hotel room, the password is ${final_convert}`)
     }   
 let convertVocal = word=>{
     let expresionRegular = /[a-zA-Z]+/.test(word)  
@@ -118,30 +159,39 @@ let game = ()=>{
     alert("Taxi Driver:challingi icciptid ⚔️")
    let ramdonNumber = randomNumber(1,3)
     alert("📄🪨✂️Lets play Scissor Paper and Rock 🍀🍀🍀")
-    let opcion = parseInt(prompt("Choose between \n 1: 📄paper \n 2: 🪨rock \n 3:✂️scissors "))
+    
     let driverChoose,scissors,rock,paper,messaje
     let lost = 1
     let won = 1
     let tie = 1
+    let flag = true
+    let opcion
+    while(flag){
+    opcion = parseInt(prompt("Choose between \n 1: 📄paper \n 2: 🪨rock \n 3:✂️scissors "))
+    
     if(!isNaN(opcion) && opcion >0 && opcion <=3){
           // Taxi Driver choose
         if(ramdonNumber === 1 ){
             driverChoose = "The taxi driver choose paper📄"
             paper = 1
+            flag = false
         }
         else if(ramdonNumber === 2){
             driverChoose = "The taxi driver choose rock🪨 "
             rock =2
+            flag = false
         }   
         else{
             driverChoose = "The taxi driver choose scissors✂️"
             scissors =3
+            flag = false
         }
    
     }
     else{
-        alert("Enter valid opcion")
-        return
+        alert("Enter valid opcion") 
+        
+    }
     } 
    if(scissors === opcion || rock === opcion || paper == opcion){
         messaje = "a tie"
@@ -190,68 +240,87 @@ let game = ()=>{
 // each deciction is about the dress color with them we can finally the results
 let yellow = ()=>{
     alert("Hildebrando has chosen yellow💛, 🏊‍♂️🏊‍♀️he will go to the swimming pool,")
-    let opcion = parseInt(prompt(("The water smells strange, What do you want to do?\n 1 to swim🏊\n any do do other thing")))
+    let flagIn = true
+    while(flagIn){
+    let opcion = parseInt(prompt(("The water smells strange, What do you want to do?\n 1 to swim🏊\n any number to do other thing")))
     if(!isNaN(opcion)){
         if(opcion === 1) {
+
+
+            
            alert("Hildebrando starts to feel suffocated, too much chlorine, he dies 💀💀💀")
            characteristics.live = false
            flag = false
+           flagIn = false
         }
         else{
             alert("Nothing happen")
+            flagIn = false
         }
         
     }
     else(
         alert("Please enter a valid Opcion")
     )
+}
 
 }
 let blue =()=>{
     alert("Hildebrando has chosen Blue 🔵,you will go to do an activity in the hotel,")
-    let opcion = parseInt(prompt(("What do you want to do?\n 1: to play bingo 🎱\n 2: to dance💃🕺 \n press any ley: Gambling in the Casino 🎰")))
+    let flagIn = true
+    while(flagIn){
+    let opcion = parseInt(prompt(("What do you want to do?\n 1: to play bingo 🎱\n 2: to dance💃🕺 \n press any number: Gambling in the Casino 🎰")))
     if(!isNaN(opcion)){
         if(opcion === 1) {
            alert("Hildebrando won the bingo, Total prize $1.000.000 🤑💸")
            characteristics.budget += 1000000;
+           flagIn = false
         }
         else if (opcion === 2){
             alert("Hildebrando Passed a great day dancing 💃🕺 with a beatiful girl")
+            flagIn = false
         }
         else{
-            alert("Hildebrando bets, and only ends up with hist return ticket, sadly his vacation is over 😢")
+            alert("Hildebrando bets, and only ends up with hist return ticket,😢")
+            let num  = characteristics.budget * 0.8
+            characteristics.budget-=num
+            spendMoney+= num
             flag = false
-
+            flagIn = false
         }
         
     }
     else(
         alert("Please enter a valid Opcion")
     )
+}
    
     
 }
 let red =()=>{
     alert("Hildebrando has chosen 🔴Red ,Hildebrando will go to the beach 🏖️")
+    let flagIn = true
+    while(flagIn){
     let opcion = parseInt(prompt(("What do you want to do?\n1: play volleyball 🏐\n2: to swim in the sea 🌊 \nPress any key:to Drink Alcohol 🍾🍾🍾 ")))
     if(!isNaN(opcion)){
         if(opcion === 1) {
            alert("Hildebrando plays voleyball 🏐, he pass a great time")
-
+           flagIn = false
            
         }
         else if (opcion === 2){
             alert("Hildebrando swins,and he drives a water scooters for $100.000")
-            characteristics.budget-= 100000
             spendMoney+=100000
+            flagIn = false
         }
         else{
             alert("Hildebrando starts drinking cocktails while resting 🥴, suddenly he feels a strong headache and begins to lose his vision  , adulterated chirrinchi, he had to go back in an emergency 🏥. he spend $500000")
             alert("Vacations is over 😔")
             characteristics.budget-=500000
             spendMoney+=500000
-            flag = false
-            
+            characteristics.live= false
+            flagIn = false
+            flag = false            
         }
         
     }
@@ -259,27 +328,34 @@ let red =()=>{
         alert("Please enter a valid Opcion")
     )
 }
+}
 let green =()=>{
     alert("Hildebrando has chosen green 🟢,he will hike and take with him water,🚶‍♂️🚶‍♀️")
-    let opcion = parseInt(prompt(("Hildebrando, What do you want to do?\n1: go all the hike with the group\npress any key:to go to the hike alone ")))
+    let flagIn = true
+    while(flagIn){
+    let opcion = parseInt(prompt(("Hildebrando, What do you want to do?\n1: go all the hike with the group🌱\npress any key:to go to the hike alone 🌱")))    
     if(!isNaN(opcion)){
         if(opcion === 1) {
-           alert("Hildebrando goes to a beautiful waterfall and take photos.")
-           
+           alert("Hildebrando goes to a beautiful waterfall and take photos.")  
+           flagIn = false
         }
         else{
-            alert("He will reach a certain point, and will return alone, and at night gets lost 😨")
+            alert("He will reach a certain point, and will return alone, 🌱 and at night gets lost 😨 , Hildebrando was eaten by a wolf 🐺")
             alert("Vacations is over 😔")
+            live = false
             flag = false
+            flagIn = false
         }    
     }
     else(
         alert("Please enter a valid Opcion")
     )
 }
+}
 let vacationsActivities = ()=>{
     while(counter <4 && flag){
-     counter+=1   
+     counter+=1  
+     data() 
     if(counter === 1){
         alert("🌴🏖️🌞Today is Saturday, Day 1🍹🕶️🏄‍♂️")
     }
@@ -319,21 +395,18 @@ let vacationsActivities = ()=>{
             }  
 // Results 
 let results = ()=>{
-    alert("The Vacation is over, these are the results")
+    alert("The Vacation is over,these are the results")
     alert(`Hildebrando was for ${counter} days`)
     let resultLive = characteristics.live
     if(resultLive){
-        alert("Hildebrado is alive")
+        alert("Hildebrado is alive and he returned to Bogota")
     }
     else{
-        alert("Hildebrado is Dead for his bad choises")
+        alert("Hildebrado is Dead for your bad choises")
     }
 
-    alert(`The total money  spend by Hildebardo was ${spendMoney}`)
-
-
+    alert(`The total money spend by Hildebardo was ${spendMoney}`)
 }            
-
 function main(){
     alert("Hildebrando is packing his bags🧳🧳, Day thursday")
     data()
@@ -342,19 +415,23 @@ function main(){
     characteristics.budget+=2500000
     data()
     staringTrip()
+    medellinStop()
+    data()
     descryptBinary()
     alert("Upon arriving in Macondo, he realizes that everyone speaks differently, only using the vowel (i). So, everything he will say is with the vowel (i). To request a taxi, it would be converted ")
     let word = "Taxi would you mind to take me to butterflies yellow "
     convertVocal(word)
-    convertVocal(word)
     data()
-    alert("Finally , he can take a taxi")
+    alert("Finally ,he could take a taxi🚖")
     game()
     data()
     alert("He arrives at the hotel and notices that for the activities inside the hotel they have a dress code for everything,") 
+    data()
     vacationsActivities()
     results()
-
+    data()
+    
 }
-
+   
+   
 main()
